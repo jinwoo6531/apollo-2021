79aaa93bf7e6972efefa29212c9aca5e998771c6
jinwoo <jintakk6531@gmail.com>
2021-02-10 21:37:24 +0900

    2/10

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIES = gql`
+{
+  movies {
+    id
+    medium_cover_image
+  }
+}
+`
 
 const Home = () => {
+    const { loading } = useQuery(GET_MOVIES); 
     return (
         <div>
           !  
